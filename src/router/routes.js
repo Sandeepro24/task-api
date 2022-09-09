86285c32c739ef50b366a18383b242d2a1af31e5
sandeep jasawat <jasawatsandeep31@gmail.com>
2022-09-09 19:23:11 +0530

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-// http://localhost:8080/v1
+// http://localhost:3000/v1/task
 
 // Todo
 // GET and POST
 
-app.use("/task", require("../api/task"));
+app.use("/task", require("../api/task")); 
 
 module.exports = app;
