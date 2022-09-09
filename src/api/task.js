6c2b856de4cf5fbcd82d6b974d7a14f5e75e55fb
sandeep jasawat <jasawatsandeep31@gmail.com>
2022-09-09 19:18:10 +0530

    Update task.js

--- src/api/task.js
+++ src/api/task.js
@@ -1,25 +1,29 @@
+// Requiring modules
 const express = require("express");
 const router = express();
 const Todo = require("../database/mongo/schema/todo.schema");
 
+
+//Making Get request to get the list of all task
 router.route("/list").get(async (req, res) => {
   try {
-    const getList = await Todo.find({});
-    res.status(200).json({ data: getList, is_success: true });
+    const getList = await Todo.find({}); // it will find all the task
+    res.status(200).json({ data: getList, is_success: true }); // sending all task as list to the client
   } catch (error) {
-    res.status(500).json({ message: error.message, is_success: false });
+    res.status(500).json({ message: error.message, is_success: false }); 
   }
 });
+
+//Post method to add task to the database
 router.route("/add").post(async (req, res) => {
-  //destructuring
-  const { taskName, taskDescription, creator, duration } = req.body;
+  const { taskName, taskDescription, creator, duration } = req.body;////destructuring
   try {
-    const task = await new Todo({
+    const task = await new Todo({ //creating new task
       taskName,
-      taskDescription,
+      taskDescription,    //assigning values what we got from through post method
       creator,
       duration,
-    }).save();
+    }).save();  // saving to the database
     res.status(200).json({ data: task, is_success: true });
   } catch (error) {
     res.status(500).json({ error: error.message, is_success: false });
